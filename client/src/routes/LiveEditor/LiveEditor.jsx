69e24e2c8e3cdd269818b9c846657f71f3d35beb
nitzanpap <nitzanpap@gmail.com>
2023-01-03 22:33:20 +0200

    Basic static code highlighting works

--- client/src/routes/LiveEditor/LiveEditor.jsx
+++ client/src/routes/LiveEditor/LiveEditor.jsx
@@ -1,13 +1,24 @@
 import { useParams } from 'react-router-dom';
 import styles from './LiveEditor.module.scss';
+import hljs from 'highlight.js';
+import 'highlight.js/styles/github-dark.css';
+import { useEffect } from 'react';
+
+const codeBlock = '// Example code:\nconst x = 10 + 5;\nconsole.log(x); // 15';
 
 function LiveEditor() {
   const { name } = useParams();
+
+  useEffect(() => {
+    hljs.highlightAll();
+  });
   return (
     <section className={styles.LiveEditor}>
       <h1>Live Editor</h1>
-      <p>{name}</p>
-      {console.log(name)}
+      <h4>{name}</h4>
+      <pre className={styles.pre}>
+        <code className="language-javascript">{codeBlock}</code>
+      </pre>
     </section>
   );
 }
