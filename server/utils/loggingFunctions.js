df272c71b07ec3ff0380811217f0a8221c6e2757
Nitzan Papini <nitzanpap@gmail.com>
2024-05-31 22:57:33 +0300

    More logs

--- server/utils/loggingFunctions.js
+++ server/utils/loggingFunctions.js
@@ -9,3 +9,9 @@ export function printUserSocket(isMentor, id) {
     ? console.log(`${clc.yellow('Mentor')} connected: ${clc.greenBright(id)}`)
     : console.log(`${clc.magenta('Student')} connected: ${clc.cyan(id)}`);
 }
+
+export const replaceMostStrWithAsterisks = (str) => {
+  const firstChar = str[0];
+  const lastChar = str[str.length - 1];
+  return firstChar + '*'.repeat(str.length - 2) + lastChar;
+};
